9f1ead889b6c694e171985cf081ec13d29f40b70
jdamanalo <jdamanalo@gmail.com>
2016-03-30 23:23:41 +0800

    Updated landing page UI and functionality

--- components/studEx/studEx.controller.js
+++ components/studEx/studEx.controller.js
@@ -1,18 +1,33 @@
 'use strict';
 
+var config
+
 (function () {
 	var app = angular.module('studEx');
 	app.controller('studExCtrl', studExCtrlFunc);
 
-	studExCtrlFunc.$inject = ['$scope'];
+	studExCtrlFunc.$inject = ['$scope', '$http'];
 	
-	
-
-	function studExCtrlFunc($scope) {
+	function studExCtrlFunc($scope, $http) {
 		$scope.form = 'choice';
 		$scope.user = {};
+
+		$scope.register = function () {
+			$http.post(config.backend_url + '/user', $scope.user).then(success, error);
+
+			function success (response) {
+				console.log(response);
+			};
+
+			function error (response) {
+				console.log(response);
+			}
+		};
+	
+		$scope.login = function () {
+
+		};
+
 	};
-
-
 
 })();
