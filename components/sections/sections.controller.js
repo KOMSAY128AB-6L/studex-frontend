d36823e382cc11ed384179eb4a5186478ebde1df
cquintos <jdquintos@up.edu.ph>
2016-04-17 20:12:04 +0800

    initial ui of sections page

---
+++ components/sections/sections.controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function () {
+	var app = angular.module('studEx');
+	app.controller('sectionsCtrl', sectionsCtrlFunc);
+
+	sectionsCtrlFunc.$inject = ['$scope', '$http'];
+	
+	function sectionsCtrlFunc($scope, $http) {
+
+	};
+
+})();
--- studEx.js
+++ studEx.js
@@ -22,6 +22,10 @@
 				'templateUrl': './components/history/history.view.html',
 				'controller' : 'historyCtrl'
 			})
+			.when('/sections', {
+				'templateUrl': './components/sections/sections.view.html',
+				'controller' : 'sectionsCtrl'
+			})
 			.otherwise({'redirectTo': '/'})
 	}
 
