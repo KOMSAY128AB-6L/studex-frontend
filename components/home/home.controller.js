f5899e5f21aabb468a39f7027a2fc9c51c6215a9
jdamanalo <jdamanalo@gmail.com>
2016-04-27 09:19:49 +0800

    Added http request for sections in home

--- components/home/home.controller.js
+++ components/home/home.controller.js
@@ -5,10 +5,31 @@
 	app.controller('homeCtrl', homeCtrlFunc);
 	
 
-	homeCtrlFunc.$inject = ['$scope', '$http', '$location', '$mdToast'];
+	homeCtrlFunc.$inject = ['$scope', '$http', '$location', '$mdToast', 'navbarService'];
 
-	function homeCtrlFunc($scope, $http, $location, $mdToast) {
+	function homeCtrlFunc($scope, $http, $location, $mdToast, navbarService) {
 		$scope.title = 'RANDOMIZE';
+
+		$scope.navigation = navbarService.navigation();
+
+		$http({
+				method: 'GET',
+				url: 'http://' + config.backend_url + '/classes',
+				data: $scope.user,
+				withCredentials:true
+			}).then(success, error);
+
+		function success (response) {
+			$scope.classes = response.data.data.items;
+		};
+
+		function error (response) {
+			$mdToast.show(
+				$mdToast.simple()
+					.textContent(response.data.errors[0].message)
+					.hideDelay(1000)
+            );
+		}
 	};
 
 })();
