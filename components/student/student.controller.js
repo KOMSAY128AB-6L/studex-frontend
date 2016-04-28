2c2e5d705ea229b8241f0691afd85ddcacd38882
najaafar <nurfitrajaafar@gmail.com>
2016-04-28 12:27:15 +0800

    Updated add student

---
+++ components/student/student.controller.js
@@ -0,0 +1,41 @@
+'use strict';
+
+(function () {
+	var app = angular.module('studEx');
+	app.controller('studentCtrl', studentCtrlFunc);
+
+	studentCtrlFunc.$inject = ['$scope', '$http', '$location', '$mdToast'];
+	
+	function studentCtrlFunc($scope, $http, $location, $mdToast) {
+
+		$scope.add = function () {
+			$http({
+				method: 'POST',
+				url: 'http://' + config.backend_url + '/student/:id',
+				data: $scope.user,
+				withCredentials:true
+			}).then(success, error);
+
+			console.log($scope.user);
+
+			function success (response) {
+				$mdToast.show(
+					$mdToast.simple()
+						.textContent('Successfully added!')
+						.hideDelay(1000)
+                );
+			};
+
+			function error (response) {
+				$mdToast.show(
+					$mdToast.simple()
+						.textContent(response.status)
+						.hideDelay(1000)
+                );
+			}
+		};
+
+
+	};
+
+})();
