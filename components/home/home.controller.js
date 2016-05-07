627f19b373a9b8428e23aa4bd507eb9f7ea01986
jdamanalo <jdamanalo@gmail.com>
2016-05-07 19:24:21 +0800

    Added support for tags

--- components/home/home.controller.js
+++ components/home/home.controller.js
@@ -24,19 +24,17 @@
 				method: 'GET',
 				url: config.protocol + config.backend_url + '/classes',
 				withCredentials:true
-			}).then(success, error);
-
-		function success (response) {
-			$scope.classes = response.data.data.items;
-		};
-
-		function error (response) {
-			$mdToast.show(
-				$mdToast.simple()
-					.textContent(response.data.errors[0].message)
-					.hideDelay(1750)
-            );
-		};
+			}).then(
+			function (response) {
+				$scope.classes = response.data.data.items;
+			}, 
+			function (response) {
+				$mdToast.show(
+					$mdToast.simple()
+						.textContent(response.data.errors[0].message)
+						.hideDelay(1750)
+            	);
+			});
 
 		$scope.selectClass = function () {
   			$scope.data.student_list = [];
@@ -44,19 +42,20 @@
   			angular.forEach($scope.classes, function (value, key) {
 
   				if (!!value.selected) {
+  					
   					if (!!$scope.data.no_called) {
   						$http({
 	  						method: 'GET',
 	  						url: config.protocol + config.backend_url + '/repetition/' + value.class_id,
 	  						withCredentials: true
 	  					}).then(
-	  					(response) => {
+	  					function (response) {
 	  						response.data.data.items.forEach((student) => {
 	  							var stud_obj = {student_id: student.student_id};
 	  							$scope.data.student_list.push(stud_obj);
 	  						});
 	  					}, 
-	  					(response) => {
+	  					function (response) {
 	  						$mdToast.show(
 								$mdToast.simple()
 									.textContent(response.data.errors[0].message)
@@ -70,13 +69,13 @@
 	  						url: config.protocol + config.backend_url + '/student/' + value.class_id,
 	  						withCredentials: true
 	  					}).then(
-	  					(response) => {
-	  						response.data.data.items.forEach((student) => {
+	  					function (response) {
+	  						response.data.data.items.forEach(function (student) {
 	  							var stud_obj = {student_id: student.student_id};
 	  							$scope.data.student_list.push(stud_obj);
 	  						});
 	  					}, 
-	  					(response) => {
+	  					function (response) {
 	  						$mdToast.show(
 								$mdToast.simple()
 									.textContent(response.data.errors[0].message)
@@ -89,14 +88,20 @@
 		
 		};
 
+		$scope.inputTags = function () {
+			if (!!$scope.data.tags_input) {
+				$scope.data.tags = $scope.data.tags_input.split(/,\s*/);
+			}
+		}
+
 		$scope.getVolunteers = function () {
 			$scope.data.settings.byChance = false;
 			$scope.data.settings.byCount = false;
 
-			if ($scope.data.group1 === 'chance') {
+			if ($scope.data.randomize === 'chance') {
 				$scope.data.settings.byChance = true;
 			}
-			else if ($scope.data.group1 === 'count') {
+			else if ($scope.data.randomize === 'count') {
 				$scope.data.settings.byCount = true;
 			}
 
@@ -106,10 +111,10 @@
 				data: $scope.data,
 				withCredentials: true
 			}).then(
-			(response) => {
+			function (response) {
 				$scope.students = response.data.data.items;
 			}, 
-			(response) => {
+			function (response) {
 				$mdToast.show(
 				$mdToast.simple()
 					.textContent(response.data.errors[0].message)
