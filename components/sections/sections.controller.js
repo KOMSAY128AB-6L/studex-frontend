93229f8d2916ec11fc1fa468a29bc4d2b305d00a
jdamanalo <jdamanalo@gmail.com>
2016-05-09 13:42:16 +0800

    Integrated view section to sections

--- components/sections/sections.controller.js
+++ components/sections/sections.controller.js
@@ -4,15 +4,114 @@
 	var app = angular.module('studEx');
 	app.controller('sectionsCtrl', sectionsCtrlFunc);
 
-	sectionsCtrlFunc.$inject = ['$scope', '$http', '$location', '$mdToast', 'navbarService'];
+	sectionsCtrlFunc.$inject = ['$scope', '$http', '$location', '$mdToast', 'navbarService', 'authService'];
 	
-	function sectionsCtrlFunc($scope, $http, $location, $mdToast, navbarService) {
+	function sectionsCtrlFunc($scope, $http, $location, $mdToast, navbarService, authService) {
+		authService.auth();
+
+		$scope.config = config;
+		$scope.check = false;
 		$scope.isHidden = true;
 		$scope.csvHide = true;
 		$scope.navigation = navbarService.navigation();
 		$scope.class = {};
+		$scope.section = {};
 		$scope.title = "SECTIONS";
 		
+		$scope.viewSection = function (section) {
+			$scope.title = section.class_name + section.section;
+			$scope.form = 'viewSection';
+
+			$scope.section.className = section.class_name;
+			$scope.section.section   = section.section;
+			$scope.section.id        = section.class_id.toString();
+
+			$http({
+				method: 'GET',
+				url: config.protocol + config.backend_url + '/class/' + section.class_id,
+			  	withCredentials: true
+			}).then(
+			function (response) {
+				$scope.class = response.data.data.items;
+		  		$scope.flag = [];
+			},
+			function (response) {
+				$mdToast.show(
+			  		$mdToast.simple()
+			  			.textContent("No students")
+			  			.hideDelay(3000)
+			  	);
+			});
+			
+			$scope.class_info = section;
+		}
+
+		$scope.editSection = function(){
+			$scope.check = true;
+		}
+
+		$scope.saveSection = function(){
+			$scope.check = false;
+
+			var data = $scope.section;
+			
+			$http({
+				method: 'PUT',
+				url: config.protocol + config.backend_url + '/class',
+				data: data,
+				withCredentials:true
+			}).then(
+			function (response) {
+				$mdToast.show(
+					$mdToast.simple()
+						.textContent('Successfully updated class!')
+						.hideDelay(1000)
+                );
+			}, 
+			function (response) {
+				$mdToast.show(
+					$mdToast.simple()
+						.textContent(response.data.errors[0].message)
+						.hideDelay(3000)
+                );
+			});
+		}
+
+		$scope.editStudent = function(index) {
+		  	$scope.flag[index] = true;
+		};
+
+		$scope.saveStudent = function(index) {
+			$scope.flag[index] = false;
+			
+			var data = $scope.class[index];
+			var temp_id = $scope.class[index].student_id;
+
+			data.chance = parseFloat(data.chance);
+			
+			$http({
+				method: 'PUT',
+				url: config.protocol + config.backend_url + '/student/' + temp_id,
+				data: data,
+				withCredentials:true
+			}).then(
+			function (response) {
+				$mdToast.show(
+					$mdToast.simple()
+						.textContent('Successfully updated student!')
+						.hideDelay(1000)
+                );
+			}, 
+			function (response) {
+				$mdToast.show(
+					$mdToast.simple()
+						.textContent(response.data.errors[0].message)
+						.hideDelay(3000)
+                );
+			});
+			
+		}
+
 		$scope.uploadSection = function () {
 			$scope.isHidden = !$scope.isHidden;
 		}
@@ -31,19 +130,18 @@
 
 		$http({
 			method: 'GET',
-			url: 'http://' + config.backend_url + '/classes',
+			url: config.protocol + config.backend_url + '/classes',
 			withCredentials:true
 		}).then(success, error);
 
 		function success (response) {
-			console.log(response);
 			$scope.classes = response.data.data.items;
 		};
 
 		function error (response) {
 			$mdToast.show(
 				$mdToast.simple()
-					.textContent(response.status)
+					.textContent(response.data.errors[0].message)
 					.hideDelay(1000)
 							);
 		}
@@ -51,7 +149,7 @@
 		$scope.add_class = function () {
 			$http({
 				method: 'POST',
-				url: 'http://' + config.backend_url + '/class',
+				url: config.protocol + config.backend_url + '/class',
 				data: $scope.class,
 				withCredentials: true
 			}).then(success, error);
@@ -61,7 +159,6 @@
 			};
 
 			function error (response) {
-				console.log(response);
 				$mdToast.show(
 				$mdToast.simple()
 						.textContent(response.data.errors[0].message)
@@ -69,6 +166,8 @@
 	          	);
 			}
 		};
+
+
 	};
 
 })();
