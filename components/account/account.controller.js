a2e931c3acf215a3378a90156db986802d3cfa6d
mamerisawesome <atmalmer23@gmail.com>
2016-05-19 12:17:45 +0800

    Added front-end validation for user email on update

--- components/account/account.controller.js
+++ components/account/account.controller.js
@@ -106,30 +106,38 @@
 		};
 
 		$scope.saveProfile = function() {
-			$http({
-				method: 'PUT',
-				url: config.protocol + config.backend_url + '/teacher',
-				data: $scope.temp,
-				withCredentials:true
-			}).then(success, error);
+			if (/[^\s@]+@[^\s@]+\.[^\s@]+/.test($scope.temp.email)) {
+				$http({
+					method: 'PUT',
+					url: config.protocol + config.backend_url + '/teacher',
+					data: $scope.temp,
+					withCredentials:true
+				}).then(success, error);
 
-			function success (response) {
+				function success (response) {
+					$mdToast.show(
+						$mdToast.simple()
+							.textContent('Successfully updated profile!')
+							.hideDelay(3000)
+	                );
+					authService.setSession($scope.temp, function (){});
+					$scope.form = 'home';
+					$scope.title = 'MY ACCOUNT';
+				};
+
+				function error (response) {
+					$mdToast.show(
+						$mdToast.simple()
+							.textContent(response.data.errors[0].message)
+							.hideDelay(3000)
+	                );
+				}
+			} else {
 				$mdToast.show(
 					$mdToast.simple()
-						.textContent('Successfully updated profile!')
+						.textContent('Invalid email format')
 						.hideDelay(3000)
-                );
-				authService.setSession($scope.temp, function (){});
-				$scope.form = 'home';
-				$scope.title = 'MY ACCOUNT';
-			};
-
-			function error (response) {
-				$mdToast.show(
-					$mdToast.simple()
-						.textContent(response.data.errors[0].message)
-						.hideDelay(3000)
-                );
+				);
 			}
 		}
 
